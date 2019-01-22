796ca13f621add5e1905a6dad79b283a46e55c23
rahulagarwal991 <46845240+rahulagarwal991@users.noreply.github.com>
2019-01-22 22:21:12 +0530

    Initial commit

---
+++ app/src/main/java/com/learn/bird/MainActivity.java
@@ -0,0 +1,21 @@
+package com.learn.bird;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    public void fade(View view) {
+        ImageView bird=(ImageView) findViewById(R.id.bird);
+        bird.animate().alpha(0f).setDuration(2000);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
